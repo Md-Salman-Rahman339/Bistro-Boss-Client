b37bdccf0c565d154214868a66753f99c800dda0
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-04-25 20:08:50 +0600

    create menu page and set dynamic page title based on route

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async';
 import {
  
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+<React.StrictMode>
+<HelmetProvider>
   <div className='max-w-6xl mx-auto'>
        <RouterProvider router={router} />
      </div>
-  </StrictMode>,
+  </HelmetProvider>
+</React.StrictMode>,
 )
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const navOptions = <>
-         <li><a>Home</a></li>
+         <li><Link to="/">Home</Link></li>
+         <li><Link to="/menu">Menu</Link></li>
       
          
      </>
