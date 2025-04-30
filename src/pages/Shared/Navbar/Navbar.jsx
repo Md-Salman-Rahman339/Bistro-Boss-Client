7dd90df2742d254cae4e2bbe6b589bdcd9df628a
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-04-30 21:25:49 +0600

    created dashboard route and sidebar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
          {/* <li><Link to="/order">Order Food</Link></li> */}
          <li><Link to="/order/salad">Order Food</Link></li>
          <li><Link to="/secret">Secret</Link></li>
-         <li><Link to="/"><button className='btn'> <FaShoppingCart className='mr-2'></FaShoppingCart>  <div className="badge badge-secondary">+{cart.length}</div> </button></Link></li>
+         <li><Link to="/dashboard/cart"><button className='btn'> <FaShoppingCart className='mr-2'></FaShoppingCart>  <div className="badge badge-secondary">+{cart.length}</div> </button></Link></li>
 
          {
              user ? <>
