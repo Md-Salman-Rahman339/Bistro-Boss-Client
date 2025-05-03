9d69036088267a64b43e6870cf8f97d4b94d909c
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-05-03 20:00:49 +0600

    created admin route

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -6,15 +6,15 @@ import { useQuery } from "@tanstack/react-query";
  const useAdmin = () => {
      const { user } = useAuth();
      const axiosSecure = useAxiosSecure();
-     const { data: isAdmin } = useQuery({
-         queryKey: [user?.email, 'isAdmin'],
+     const { data: isAdmin,isPending: isAdminLoading } = useQuery({
+         queryKey: [user?.email, 'isAdmin' ],
          queryFn: async () => {
              const res = await axiosSecure.get(`/users/admin/${user.email}`);
              console.log(res.data);
              return res.data?.admin;
          }
      })
-     return [isAdmin]
+     return [isAdmin,isAdminLoading]
  };
  
  export default useAdmin;
