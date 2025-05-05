2aaee03e1cb0a1102761af4f2dcb9d95ae1c91f3
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-05-05 20:18:12 +0600

    pload image to image hosting server imgbb and get image url

--- src/Dashboard/AddItems/AddItems.jsx
+++ src/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { FaUtensils } from 'react-icons/fa';
+import useAxiosPublic from '../../hooks/useAxiosPublic';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
+const image_hosting_key=import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
     const {register,handleSubmit}=useForm();
-    const onSubmit=(data)=>{
-        console.log(data)
+    const axiosPublic=useAxiosPublic();
+    const onSubmit= async(data)=>{
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        });
+        console.log(res.data)
     }
   return (
     <div>
@@ -29,9 +41,9 @@ const AddItems = () => {
                              <label className="label">
                                  <span className="label-text">Category*</span>
                              </label>
-                             <select {...register('category', {required: true})}
+                             <select defaultValue="default" {...register('category', {required: true})}
                                  className="select select-bordered w-full">
-                                 <option disabled selected>Select a category</option>
+                                 <option disabled value="default">Select a category</option>
                                  <option value="salad">Salad</option>
                                  <option value="pizza">Pizza</option>
                                  <option value="soup">Soup</option>
