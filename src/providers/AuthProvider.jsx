334a44b84f56033e925f0b814769195c92c6a0e1
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-05-09 21:28:34 +0600

    display admin stats

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -48,13 +48,15 @@ import useAxiosPublic from "../hooks/useAxiosPublic";
                  .then(res=>{
                     if(res.data.token){
                         localStorage.setItem('access-token',res.data.token)
+                        setLoading(false);
                     }
                  })
              }
              else{
                 localStorage.removeItem('access-token');
+                setLoading(false);
              }
-             setLoading(false);
+            
          });
          return () => {
              return unsubscribe();
